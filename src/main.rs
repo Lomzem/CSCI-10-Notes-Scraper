b95f21157d9dedd922d6c3ec1ba4defea4e6dde6
Lomzem <lawjaylee1@icloud.com>
2024-04-09 09:48:26 -0700

    second request

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ struct Module {
     name: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct LectureNote {
+    id: u32,
+    name: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let access_key = std::env::var("CANVAS_ACCESS_KEY").expect("Unable to find canvas key");
@@ -18,31 +24,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_params = [("access_token", access_key)];
 
     let modules_url = Url::parse_with_params(
-        canvas_url.join(modules_endpoint.as_ref())?
-            .as_str(),
+        canvas_url.join(modules_endpoint.as_ref())?.as_str(),
         &api_params,
     )?;
 
-    let module_list: Vec<Module> = reqwest::get(modules_url)
-        .await?
-        .json()
-        .await?;
+    let module_list: Vec<Module> = reqwest::get(modules_url).await?.json().await?;
 
     let notes_module: &Module = module_list.last().unwrap();
     let list_module_endpoint = format!(
-        "GET /api/v1/courses/{}/modules/{}/items",
-        CSCI_10_ID,
-        notes_module.id,
+        "/api/v1/courses/{}/modules/{}/items",
+        CSCI_10_ID, notes_module.id,
     );
 
     let list_module_url = Url::parse_with_params(
-        canvas_url.join(list_module_endpoint.as_ref())?
-            .as_str(),
+        canvas_url.join(list_module_endpoint.as_ref())?.as_str(),
         &api_params,
-    );
+    )?;
 
-    println!("{:?}", notes_module);
-    // std::fs::write("response.json", body?.as_bytes())?;
+    let module_contents = reqwest::get(list_module_url).await?.bytes().await?;
+
+    // println!("{:?}", module_contents);
+    std::fs::write("response.json", module_contents)?;
 
     Ok(())
 }
